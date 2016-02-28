50df195edf72071657dffc75de307783274fd852
Swyter <swyterzone@gmail.com>
2016-02-28 19:29:43 +0100

    Initial version under revision control

---
+++ sphinx_filelistbin_extract.c
@@ -0,0 +1,22 @@
+/* asdf */
+
+#include <stdio.h>
+
+void main(int argc, char *argv[])
+{
+	puts("\n"
+	     "  SPHINX AND THE CURSED MUMMY «FILELIST» EXTRACTOR              \n"
+		 "  --------------------------------------------------------------\n"
+	     "  (C) 2016-02-28 Created by Swyter <swyterzone+sphinx@gmail.com>\n"
+	     "\n"
+	     "  Licensed under MIT-like terms, this is a FOSS program.        \n");
+
+	if (argc != 3)
+	{
+		printf("  > USAGE: %s <filelist.bin descriptor> <filebin.000 container> \n\n", argv[0]);
+		return;
+	}
+	
+	
+	
+}
