ce09bff468e3b1deba3d14ebcb76a0866e3f5244
Swyter <swyterzone@gmail.com>
2016-02-28 20:14:36 +0100

    Start fleshing out the program proper

--- sphinx_filelistbin_extract.c
+++ sphinx_filelistbin_extract.c
@@ -1,6 +1,30 @@
-/* asdf */
+/* Structs of the filelist.bin file were reverse engineered with 010 editor from
+   the contents stored in filelist.txt, also included in the GCN disk.
+   
+   Should work with various versions and platforms of Eurocom's EngineX with little work */
 
+#include <stdint.h>
 #include <stdio.h>
+
+struct header
+{
+	uint8_t magic;
+	uint8_t total_size; // <format=hex>;
+	uint8_t list_item_count;
+	uint8_t unk_b; // <format=hex>;
+	uint8_t next_section; // <format=hex>; // + 10h (address relative to this field)
+};
+
+struct list_item
+{
+    uint8_t len;
+    uint8_t hash; // <format=hex>;
+    uint8_t ver;
+    uint8_t unk_flags; //<format=hex>;
+    uint8_t always_one;
+    uint8_t loc_addr; //<format=hex>;
+    uint8_t loc_file;
+};
 
 void main(int argc, char *argv[])
 {
@@ -11,12 +35,13 @@ void main(int argc, char *argv[])
 	     "\n"
 	     "  Licensed under MIT-like terms, this is a FOSS program.        \n");
 
-	if (argc != 3)
+	if (argc != 2)
 	{
-		printf("  > USAGE: %s <filelist.bin descriptor> <filebin.000 container> \n\n", argv[0]);
+		printf("  This program extracts game files from filebin.0XX containers \n"
+		       "  by reading the same filelist.bin descriptor used by the game.\n"
+	           "\n"
+		       "  > USAGE: %s <filelist.bin>\n\n", argv[0]);
 		return;
 	}
-	
-	
-	
+
 }
