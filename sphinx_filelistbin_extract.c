5e1527eea707abd63ae72bb8ce4ab4d20a4d5862
Swyter <swyterzone@gmail.com>
2016-02-29 12:53:54 +0100

    Add a extract function to modularize a bit the entire thing.

--- sphinx_filelistbin_extract.c
+++ sphinx_filelistbin_extract.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -34,6 +35,8 @@ struct list_item
     uint32_t loc_addr; //<format=hex>;
     uint32_t loc_file;
 };
+
+void extract_to(char *descriptor, char *filename, uint32_t loc_addr, uint32_t loc_file);
 
 void main(int argc, char *argv[])
 {
@@ -71,7 +74,9 @@ open:
     return;
 
 extract:
-    puts("  [-] Extracting files...\n");
+    puts(
+        "  [-] Extracting files...\n"
+    );
 
     int fd = open(argv[1], O_RDONLY);
 
@@ -106,16 +111,71 @@ extract:
 
     for (struct list_item *item = map + sizeof(struct header); (size_t) item < (size_t) head + (sizeof(struct list_item) * ntohl(head->list_item_count)) ; item++, string_pointer++)
     {
+
+        char *filepath = (char *)((size_t) string_pointer + ntohl(*string_pointer));
+
         printf(
             "%-50s : Len %10u : Ver %4u : Hash %#010x : Loc %11x:%03u\n",
-            (size_t)string_pointer + ntohl(*string_pointer),
+            filepath,
             ntohl(item->len),
             ntohl(item->ver),
             ntohl(item->hash),
             ntohl(item->loc_addr),
-            ntohl(item->loc_file));
+            ntohl(item->loc_file)
+        );
+
+        extract_to(argv[1], filepath, ntohl(item->loc_addr), ntohl(item->loc_file));
     }
 
-    munmap(map, s.st_size);
+    munmap(map, size);
+    close(fd);
+}
+
+void extract_to(char *descriptor, char *filename, uint32_t loc_addr, uint32_t loc_file)
+{
+    char containerpath[255];
+
+    strncpy(containerpath, descriptor, sizeof(containerpath));
+
+    char *dot = strrchr(containerpath, '.') + 1;
+
+    if (!((dot - &containerpath) + sizeof("000") >= sizeof(containerpath)))
+        return;
+
+    snprintf(dot, sizeof("000"), "%03u", loc_file);
+
+    return;
+
+    //*dot++ = 0
+
+
+
+
+    int fd = open(containerpath, O_RDONLY);
+
+    if (fd < 0)
+    {
+        puts("open failed");
+        return;
+    }
+
+    struct stat s;
+    fstat(fd, &s);
+
+    size_t size = s.st_size;
+
+    void *map = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    if (map == MAP_FAILED)
+    {
+        printf("map failed: %s\n", strerror(errno));
+        return;
+    }
+
+
+    mkdir("x:", 0766);
+
+
+    munmap(map, size);
     close(fd);
 }
